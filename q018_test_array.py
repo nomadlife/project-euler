0ed0782c8946ee2bcc00cdd45608c18ed5a80f5c
nomadlife <joonwoo3816@gmail.com>
2017-08-20 19:51:36 +0900

    q018_test_array

--- q018_test_array.py
+++ q018_test_array.py
@@ -3,27 +3,51 @@
 
 # the 116033rd person to have solved this problem.
 
+
 import time
 start_time = time.time()
 
 filename = 'q018_data.txt'
 with open(filename) as file_object:
 	lines = file_object.readlines()
+'''
+for line in lines:
+	print(len(line))
+'''
 
-#for line in lines:
-#	print(len(line))
+
+
 
 i=0
-number=[]
+number=[[]]
 print(len(lines))
 while i < len(lines):
 	j=0
 	while j<len(lines[i])/3:
-		#number[i][j]=lines[i][j]+lines[i][j+1]
-		print("number({i},{j})",line[i][j],end='')
+		print(lines[i][j*3]+lines[i][j*3+1],end='')
+		#number[i][j]=int(lines[i][j]+lines[i][j+1])
+		#print(number[i][j])
 		j+=1
 	print("")
 	i+=1
+
+
+
+'''
+import numpy
+a=numpy.empty((10,10))
+a.fill(foo)
+print(a)
+'''
+
+'''
+import numpy as np
+twoD = np.array([[]*m]*n)
+twoD = np.array([[x]*m]*n)
+'''
+
+
+
 '''
 myList=[]
 for i in range(10):
