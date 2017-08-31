15b0bb9560a6d3096c5402e10be50dfee83b0ef3
nomadlife <joonwoo3816@gmail.com>
2017-08-31 23:55:22 +0900

    q019_2

--- q019.py
+++ q019.py
@@ -1,4 +1,6 @@
 
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-for i in months:
-    print(i)
+
+for year in range(1901,2001):
+    for month in months:
+        print(year,month)
