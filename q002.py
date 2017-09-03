584ec4426b5438bfcd89a1435671870fff7d00f8
nomadlife <joonwoo3816@gmail.com>
2017-09-03 15:41:13 +0900

    q002_modify

--- q002.py
+++ q002.py
@@ -1,14 +1,9 @@
-#project-euler q002
-#sum fibonachi series under 4,000,000 where even-valued term.
-
-previous_i=1;i=1;total=0
-while i<4000000 :
-	print("loop ",i)
-	if i%2==0 :
-		total=total+i
-		print("total is ",total)
-	next_i=previous_i+i
-	previous_i=i
-	i=next_i
-
-print(total)
+a=1;b=1;total=0
+while True :
+    if b>4000000 :
+        break
+    print(b,end=', ')
+    if b%2==0:
+        total = total + b
+    print(total)
+    a,b = b,a+b
