e0d104d40898cb905f805f76daa66af5a01dd966
nomadlife <joonwoo3816@gmail.com>
2017-09-03 22:30:18 +0900

    q005_update

--- q005.py
+++ q005.py
@@ -2,27 +2,14 @@
 #The smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 #LCM
 
-def get_gcd(num1,num2):
-	gcd=1
-	loop=num1
-	if num1>num2:
-		loop=num2
-	i=1
-	while i<=loop:
-		if num1%i==0 and num2%i==0:
-			gcd =i
-		i+=1
-	return gcd
-	
-i=1; gcd=0; lcm=1
-while i<20:
-	print("loop",i)
-	gcd=get_gcd(lcm,i+1)
-	print("gcd of",lcm,",",i+1," is :", gcd)
-	if gcd==0:
-		lcm = lcm*(i+1)
-	else:
-		lcm = lcm*(i+1)/gcd
-	print("lcm",lcm)
-	i=i+1
+def getGCD(num1,num2):
+    for j in range(1,num2+1):
+        if num1%j==0 and num2%j==0:
+            g=j
+    return g
 
+lcm=1
+for i in range(1,21):
+    gcd = getGCD(lcm,i)
+    lcm = lcm*i/gcd
+    print(i,gcd,lcm)
