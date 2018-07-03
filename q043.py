0f7e505ca7029daa9f3c0208fc46d48fadf8c4f6
nomadlife <joonwoo3816@gmail.com>
2018-07-03 23:39:27 +0900

    q043,44 updated

--- q043.py
+++ q043.py
@@ -1,1 +1,27 @@
 # Q043 Sub-string divisibility
+# resursion - list reverse, return summ
+def ispan(string):
+    if all(string.count(x)==1 for x in string):
+        return True
+    return False
+
+def func(level=6,string=''):
+    total=0
+    if level==6:
+        for j in lists[level]:
+            total+=func(5,j)
+        return total
+    elif level<0 :
+        for i in '0123456789':
+            if ispan(i+string):
+                total+=int(i+string)
+        return total
+    for i in lists[level]:
+        if string[:2] == i[1:] :
+            newstr=str(i[0]+string)
+            if ispan(newstr):
+                total+=func(level-1,newstr)
+    return total
+
+lists=[[str(i*j).zfill(3) for i in range(1,500) if i*j<1000] for j in [2,3,5,7,11,13,17]]        
+func()
