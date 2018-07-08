a43943b293bb3a2c8208747212a345345dc7eb5d
nomadlife <joonwoo3816@gmail.com>
2018-07-08 16:36:46 +0900

    p059 updated

--- p059.py
+++ p059.py
@@ -1,1 +1,15 @@
 # p059 XOR decryption
+keyLength=3
+for keyNum in range(keyLength):
+    string='abcdefghijklmnopqrstuvwxyz'
+    test=contents
+    maxValue =[]
+    for k in string:
+        count=0
+        for i,j in enumerate(test):
+            if i%3==keyNum:
+                val= int(j)^ord(k)
+                if chr(val) in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ':
+                    count+=1
+        maxValue.append([count,k])
+    print(max(maxValue))
