83026ac7d31116f0aaa0140e2d4e89c749c9fa87
nomadlife <joonwoo3816@gmail.com>
2018-07-13 21:07:42 +0900

    q018 updated

--- q018.py
+++ q018.py
@@ -8,8 +8,8 @@ def findSum(i=0,j=0):
     if i==14:
         return int(numbers[i][j])
     else :
-        result1 = findSum2(i+1,j)
-        result2 = findSum2(i+1,j+1)
+        result1 = findSum(i+1,j)
+        result2 = findSum(i+1,j+1)
         if result1>result2:
             return int(numbers[i][j])+result1
         else:
