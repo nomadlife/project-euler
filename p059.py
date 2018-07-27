1000d9c13476f6a19745223199ec54d7b2b69b2a
nomadlife <joonwoo3816@gmail.com>
2018-07-27 15:49:50 +0900

    p059 updated

--- p059.py
+++ p059.py
@@ -1,14 +1,21 @@
 # p059 XOR decryption
 with open('p059_cipher.txt') as file_object:
-    contents = file_object.read().rstrip().split(',')
-    
-for keyNum in range(3):
+    codes = file_object.read().rstrip().split(',')
+
+key=''
+for idx in range(3):
     maxValue =[]
     for k in range(97,123):
         count=0
-        for i,j in enumerate(contents):
-            if i%3==keyNum:
+        for i,j in enumerate(codes):
+            if i%3==idx:
                 if int(j)^k in range(65,123):
                     count+=1
         maxValue.append([count,chr(k)])
-    print(max(maxValue))
+    key+=max(maxValue)[1]
+
+total=0
+for i,v in enumerate(codes):
+    val=int(v)^ord(key[i%3])
+    total+=val
+print(total)
