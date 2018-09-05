b943c1ada958e05b3f14db763ea7f1807745912f
nomadlife <joonwoo3816@gmail.com>
2018-09-05 13:21:41 +0900

    p079 updated

--- p079.py
+++ p079.py
@@ -1,19 +1,17 @@
 #p079 Passcode derivation
 with open('p079_keylog.txt') as file:
-    codes = [line.rstrip() for line in file.readlines()]
-
-def sort(a,b):
-    if result[a]>result[b]:
-        temp = result[a]
-        result[a] = result[b]
-        result[b] = temp
+    codes = [line.strip() for line in file.readlines()]
     
-result={}
+def sort(a,b):
+    if pw[a]>pw[b]:
+        pw[a],pw[b] = pw[b],pw[a]
+
+pw={}
 for code in codes:
     for j in code:
-        if (j) not in result:
-            result[j] = len(result)+1
+        if (j) not in pw:
+            pw[j] = len(pw)+1
     sort(code[0],code[1])
     sort(code[1],code[2])
 	
-sorted(result, key=result.get)
+sorted(pw, key=pw.get)
