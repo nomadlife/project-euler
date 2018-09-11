0a8ab7a702547c963477f3a8c855c3142a679e19
nomadlife <joonwoo3816@gmail.com>
2018-09-11 10:49:11 +0900

    p015,p014 updated, code rearrange

--- q015.py
+++ q015.py
@@ -2,18 +2,11 @@
 # How many such routes are there through a 20×20 grid?
 
 def grid(r,c):
-    if c==0 or r==0:
-        return 1
-    elif r==1:
-        return c+1
-    elif c==1:
-        return r+1
-    elif (r,c) in cache:
-        return cache[r,c]
-    else:
-        cache[r,c]=grid(r,c-1)+grid(r-1,c)
-        return cache[r,c]
+    if r==1 or c==1:
+        return c+r
+    if (r,c) not in cache:
+        cache[r,c] = grid(r,c-1) + grid(r-1,c)
+    return cache[r,c]
 
 cache={}
 grid(20,20)
-print(cache[20,20])
