7b4681d84049203891a99711adae4c2903b582b6
nomadlife <joonwoo3816@gmail.com>
2018-09-13 16:15:57 +0900

    p017 full change

--- q017.py
+++ q017.py
@@ -1,40 +1,24 @@
 # Q017 Number letter counts
-# numbers 1 to 5 : one, two, three, four, five,
-# 3 + 3 + 5 + 4 + 4 = 19 letters used in total
-# how many letters would be used in numbers from 1 to 1000?
 
+data1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+       30,40,50,60,70,80,90,100,1000]
+data2='''one two three four five six seven eight nine ten 
+eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen
+twenty thirty forty fifty sixty seventy eighty ninety hundred thousand'''
+numbers=dict(zip(data1,data2.split()))
 
-filename = 'q017_data.txt'
-with open(filename) as file_object:
-	lines = file_object.readlines()
+def func(n):
+    if n <100:
+        if (n) not in numbers:
+            numbers[n] = numbers[n-n%10] + numbers[n%10]
+        return numbers[n]
+    if n <1000:
+        if n%100==0:
+            return  numbers[n//100] +  numbers[100]
+        return numbers[n//100] +  numbers[100] + 'and' + numbers[n%100]
+    return 'one' + numbers[1000]
 
-def make_string(number,string=''):
-	charNum=str(number)
-	lenNum=len(charNum)
-	if lenNum==3:
-		string=lines[int(charNum[0])-1].rstrip()+"hundred"
-		number = number - int(charNum[0]) * 100
-		if number==0:
-			return string
-		string=make_string(number,string+"and")
-	elif number<100 and number > 20:
-		string=string+lines[int(charNum[0])+17].rstrip()
-		number=number-int(charNum[0])*10
-		string=make_string(number,string)
-	elif number != 0 and number <= 20:
-		string = string+lines[number-1].rstrip()
-	return string
-
-i=1;length_i=0;total_length=0
-while i<1000:
-	print("loop:",i,end='')
-	string_i=make_string(i)
-	print(" string_i:",string_i,end='')
-	length_i=len(string_i)
-	print(" length_i:",length_i,end='')
-	total_length=total_length+length_i
-	print(" total_length:",total_length)
-	i += 1
-
-print("total_length :",total_length)
-print("add 1000(one thousand,11 letters)",total_length+11)
+total=0
+for i in range(1,1001):
+    total+=len(func(i))
+print(total)
