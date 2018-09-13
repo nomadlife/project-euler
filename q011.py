ffba908951bedd0c24c5637ce1e0ffdaff22366a
nomadlife <joonwoo3816@gmail.com>
2018-09-13 14:27:00 +0900

    p011 updated

--- q011.py
+++ q011.py
@@ -1,21 +1,17 @@
 # Q011 Largest Product in grid 20x20
 # greatest product of four adjacent numbers in the same direction
 
+import numpy as np
 with open('q011_data.txt') as f:
-	numbers = [[int(j) for j in i.split()] for i in f.readlines()]
-
-def prod(arr):
-    total =1
-    for i in arr:
-        total *= i
-    return total
+    numbers = np.array(f.read().split()).astype(int).reshape(20,20)
 
 result=[]
 for i in range(20):
     for j in range(20):
+        arr = numbers[i:i+4,j:j+4]
         result.append(max(\
-            prod(numbers[i][j:j+4]),
-            prod([numbers[i+k][j] for k in range(min(20-i,4))]),
-            prod([numbers[i+k][j+k] for k in range(min(20-i,20-j,4))]),
-            prod([numbers[i+k][j+3-k] for k in range(min(20-i,20-j-3,4))])))
+            arr[0].prod(),
+            arr[:,0].prod(),
+            arr.diagonal().prod(),
+            arr[::-1].diagonal().prod()))
 max(result)
