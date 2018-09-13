5009a69d21e4d163e36c5aa508a7fc8e1aa3cb06
nomadlife <joonwoo3816@gmail.com>
2018-09-13 16:38:34 +0900

    p017 updated

--- q017.py
+++ q017.py
@@ -1,10 +1,10 @@
 # Q017 Number letter counts
 
 data1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-       30,40,50,60,70,80,90,100,1000]
+       30,40,50,60,70,80,90]
 data2='''one two three four five six seven eight nine ten 
 eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen
-twenty thirty forty fifty sixty seventy eighty ninety hundred thousand'''
+twenty thirty forty fifty sixty seventy eighty ninety'''
 numbers=dict(zip(data1,data2.split()))
 
 def func(n):
@@ -14,9 +14,9 @@ def func(n):
         return numbers[n]
     if n <1000:
         if n%100==0:
-            return  numbers[n//100] +  numbers[100]
-        return numbers[n//100] +  numbers[100] + 'and' + numbers[n%100]
-    return 'one' + numbers[1000]
+            return  numbers[n//100] +  'hundred'
+        return numbers[n//100] +  'hundred' + 'and' + numbers[n%100]
+    return 'one' + 'thousand'
 
 total=0
 for i in range(1,1001):
