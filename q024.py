3e6e8a047c736ab9ecfb7b6f0c2f6cc9852da4b1
nomadlife <joonwoo3816@gmail.com>
2018-09-17 15:05:19 +0900

    p024 full changed

--- q024.py
+++ q024.py
@@ -1,16 +1,20 @@
 #Q024 Lexicographic permutations, find 1Mth permutation.
 
-def get_next_permutation(string):
-    i=len(string)-2
-    while i >= 0:
-        if string[i] < string[i+1]:
-            target = sorted(string[i:])
-            new_num = target.pop(target.index(string[i])+1)
-            last_nums = ''.join(target)
-            return string[:i] + new_num + last_nums
-        i-=1
+def factorial(n):
+    for i in range(1,n):
+        n*=i
+    return n
 
-string='0123456789'
-for i in range(1,1000000):
-    string = get_next_permutation(string)
-print(string)
+def permutation(n,i=0):
+    move=0
+    step = factorial(9-i)
+    while n>step:
+        n-=step
+        move+=1
+    result.insert(i,result.pop(i+move))
+    if n is not 1:
+        permutation(n,i+1)
+        
+result=list(range(10))
+permutation(1000000)
+result
