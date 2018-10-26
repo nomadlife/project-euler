3ccb70d47cd421962ad2e3d81975f4e31bb70048
nomadlife15u <aaeacd@gmail.com>
2018-10-26 15:29:47 +0900

    q019 code fix

--- q019.py
+++ q019.py
@@ -2,17 +2,15 @@
 # How many Sundays fell on the first of the month
 # during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-daylist = [31,28,31,30,31,30,31,31,30,31,30,31]
-total=0
-count=0
+totalDay = 0
+sundayCount = 0
 for y in range(1900,2001):
-    for m in range(12):
-        monthday=daylist[m]
-        if y%4==0 and m==1: 
-            monthday+=1
-        for d in range(monthday):
-            if y>1900 and d==0 and total%7==6:
-                count+=1
-            total+=1
+    for m in [31,28,31,30,31,30,31,31,30,31,30,31]:
+        if y % 4 == 0 and m == 28: 
+            m += 1
+        for d in range(m):
+            if y > 1900 and d == 0 and totalDay % 7 == 6:
+                sundayCount += 1
+            totalDay += 1
 
-print(count)
+sundayCount
