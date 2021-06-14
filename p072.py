15f942d09ddfb171ad67402add4d034811274e1f
junnushon <junnu.shon@espresomedia.com>
2021-06-14 18:32:17 +0900

    update p072

--- p072.py
+++ p072.py
@@ -1,18 +1,22 @@
 print("hello")
 
 def reduced(a,b):
-    i=2
-    while i <= a:
+    i=a
+    while i >= 2:
         if b%i==0 and a%i==0:
             b = b / i
             a = a / i
+            break
         else:
-            i+=1
+            i-=1
     return int(a),int(b)
 
 
 cache=set()
 target=1000
+import time
+start = time.time()
+
 for i in range(2,target+1):
     for j in range(1,i):
         # res = reduced(j,i)
@@ -20,6 +24,7 @@ for i in range(2,target+1):
         if (j,i) not in cache:
             cache.add(reduced(j,i))
 
-# print(cache)
-print(len(cache))
-    
+dur = time.time() - start
+print("target : ", target)
+print("answer : ", len(cache))
+print("time : ", dur)
